9a26c669b11d209bd72fcb1bf895df62338e9ae5
TomasNecesany <g00406870@atu.ie>
2024-11-23 16:13:52 +0000

    Add react-router-dom dependency and create Content component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Footer from './Components/Content';
 
 function App() {
   return (
