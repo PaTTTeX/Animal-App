cfcd4b118032dae1004a94664e526cc1633b762d
TomasNecesany <g00406870@atu.ie>
2024-11-24 04:57:07 +0000

    Add server setup with Express, Mongoose, CORS, and dotenv in server.js

---
+++ my-app/src/Components/BackEnd/server.js
@@ -0,0 +1,11 @@
+// imports
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const port = 4000;
+
+// create an express app
+const app = express();
+
