108a195b70f55fd04442ea22d11587d10660d241
TomasNecesany <g00406870@atu.ie>
2024-11-24 16:01:19 +0000

    Add middleware for CORS and connect to MongoDB in server.js

--- my-app/src/Components/BackEnd/server.js
+++ my-app/src/Components/BackEnd/server.js
@@ -9,3 +9,34 @@ const port = 4000;
 // create an express app
 const app = express();
 
+// get environment variables
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, OPTIONS"
+	);
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
+// connect to MongoDB
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+
+// schema
+
