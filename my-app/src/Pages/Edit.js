dec2b736fe97f5891933d990f05822d2046dc95b
TomasNecesany <g00406870@atu.ie>
2024-11-26 01:26:23 +0000

    Add Edit functionality: implement form for updating animal details with API integration and redirection

--- my-app/src/Pages/Edit.js
+++ my-app/src/Pages/Edit.js
@@ -26,3 +26,64 @@ export default function EditAnimal(props) {
       });
   }, [id]);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const updatedAnimal = { id, name, species, habitat, imageUrl };
+    axios.put('http://localhost:4000/api/animal/' + id, updatedAnimal)
+      .then((res) => {
+        console.log(res.data);
+        navigate('/view-animals'); // Redirect to the animals list
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center" style={{ width: '100%' }}>
+        <div className="col-md-8" style={{ margin: '1rem' }}>
+          <h3 className="text-center">Edit Animal</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Animal Name: </label>
+              <input
+                type="text"
+                className="form-control"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Species: </label>
+              <input
+                type="text"
+                className="form-control"
+                value={species}
+                onChange={(e) => setSpecies(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Habitat: </label>
+              <input
+                type="text"
+                className="form-control"
+                value={habitat}
+                onChange={(e) => setHabitat(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>Image URL: </label>
+              <input
+                type="text"
+                className="form-control"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input type="submit" value="Edit Animal" className="btn btn-primary btn-redirection"/>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
